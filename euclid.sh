6eb84e406df0930c1e1fb1fe3edead48bb93fe9c
JoeM <C21404162@mytudublin.ie>
2023-09-29 15:37:15 +0100

    First commit

---
+++ euclid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Please enter num 1: "
+read num1
+
+echo "Please enter num 2: "
+read num2
+
+a=$num1
+b=$num2
+
+while [[ $b -ne 0 ]]; do
+	temp=$b
+	b=$((a % b))
+	a=$temp
+done
+
+echo "GCD is $a"
